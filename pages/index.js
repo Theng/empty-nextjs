b4d89eae9fbfa97d665ce858160c58317b895451
unknown <neverljfe@gmail.com>
2021-10-22 13:34:36 +0700

    [TASK] init

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{height:"100vh",display:"flex", justifyContent:"center", alignItems:"center"}}>
+      <img src= "/logo-border-white.png" style={{width:200,height:200}} />
+    </div>
+  )
+}

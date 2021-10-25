3824c575e57b4d53c92c99f13e00d18e24cd6f53
unknown <neverljfe@gmail.com>
2021-10-25 11:17:27 +0700

    [TEST]

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+
 export default function Home() {
   return (
-    <div style={{height:"100vh",display:"flex", justifyContent:"center", alignItems:"center"}}>
-      <img src= "/logo-border-white.png" style={{width:200,height:200}} />
-    </div>
+    <>
+<div className="box box10"></div>
+<div className="box box9"></div>
+<div className="box box8"></div>
+<div className="box box7"></div>
+<div className="box box6"></div>
+<div className="box box5"></div>
+<div className="box box4"></div>
+<div className="box box3"></div>
+<div className="box box2"></div>
+<div className="box box1"></div>
+<div className="box box0"></div>
+<div className="title">
+<img src="/logo-border-white.png" style={{backgroundColor:"white", border:"1px solid white", borderRadius:"50%"}} width="250px" height="250px" />
+</div>
+    </>
   )
 }
